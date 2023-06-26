e00111427bd7709fcc49674bc978a0966e82c0e1
mllilek <mllilek@debian-BULLSEYE-live-builder-AMD64>
2023-06-26 01:16:50 -0400

    add a README

--- explore.py
+++ explore.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 import pprint
 import sqlite3
 
@@ -9,6 +11,8 @@ def connect():
 def _aliases(artist_id):
   result = [str(artist_id)]
   result.extend([str(x[0]) for x in con.execute(f'''select t from artist_alias where f={artist_id}''')])
+  result.extend([str(x[0]) for x in con.execute(f'''select t from artist_member where t={artist_id}''')])
+  result.extend([str(x[0]) for x in con.execute(f'''select t from artist_member where f={artist_id}''')])
   return ', '.join(result)
 
 def labels_of_artist(artist_id):
@@ -23,6 +27,15 @@ select id,name from labels where id in (
 )
   '''))
   return [(x[0], x[1], 'https://www.discogs.com/label/{}'.format(x[0])) for x in result]
+
+def releases_on_label(label_id):
+  result = []
+  result.extend(con.execute(f'''
+select id,name from releases where id in (
+  select f from release_label where t={label_id}
+)
+  '''))
+  return [(x[0], x[1], 'https://www.discogs.com/release/{}'.format(x[0])) for x in result]
 
 def releases_of_artist(artist_id):
   result = []
@@ -49,13 +62,28 @@ select id,name from artists where id in (
 
 con = connect()
 
-def walk(depth, max_depth, seen_artists, seen_labels, mode_artist, root_record):
+def walk(
+    depth,
+    max_depth,
+    seen_artists,
+    seen_labels,
+    mode_artist,
+    root_record):
   if depth == max_depth:
     return
 
   root_id = root_record[0]
 
+  # ignore massive labels like columbia
   if not mode_artist and root_id in [1818, 1866, 2311, 8377]:
+    return
+
+  if mode_artist:
+    releases_count = len(releases_of_artist(root_id))
+  else:
+    releases_count = len(releases_on_label(root_id))
+
+  if releases_count > 200:
     return
   
   if mode_artist and root_id in seen_artists:
@@ -73,9 +101,9 @@ def walk(depth, max_depth, seen_artists, seen_labels, mode_artist, root_record):
     new_seen_labels = seen_labels.union([root_id])
 
   if mode_artist:
-    frontier = labels_of_artist(root_id)[0:10]
+    frontier = labels_of_artist(root_id)
   else:
-    frontier = artists_on_label(root_id)[0:10]
+    frontier = artists_on_label(root_id)
 
   for record in frontier:
     walk(depth + 1, max_depth, new_seen_artists, new_seen_labels, not mode_artist, record)
@@ -84,7 +112,7 @@ def walk_start(max_depth, mode_artist, root):
   walk(0, max_depth, set(), set(), mode_artist, root)
 
 try:
-  walk_start(3, False, (1862921, 'Small Moves', 'https://www.discogs.com/label/1862921'))
+  walk_start(3, True, (15725, None, None))
 except BrokenPipeError:
   pass
 
